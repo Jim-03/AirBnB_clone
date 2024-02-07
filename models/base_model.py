84216a43357bcc12a67e0a79451a87d694ae4eba
Jim-03 <jcnyongesa@kabarak.ac.ke>
2024-02-07 13:47:03 +0300

    re-create an instance with a dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -6,18 +6,40 @@ from datetime import datetime
 
 class BaseModel:
     """ The base class for other classes. """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """ initializes an object.
+        Args:
+            *args: unused arguments.
+            **kwargs: arguments representing attributes.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
+        """ The string representation of object.
+        Returns:
+            string representing the object.
+        """
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """ Updates to the current date and time. """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """ The dictionary representation of the object.
+        Returns:
+            obj_dict: the dictionary representation of the object.
+        """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = type(self).__name__
         obj_dict['created_at'] = self.created_at.isoformat()
